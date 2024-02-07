99ad8f47cba4f3a7c57e453a87625e161254f955
rodelmihaid <dimuletemihai20@stud.ase.ro>
2024-02-07 16:15:51 +0200

    contact me v3 commit

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  openWhatsApp() {
+    // Verifică dacă utilizatorul este pe un dispozitiv mobil
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+      // Dacă da, deschide link-ul pentru WhatsApp
+      window.location.href = 'whatsapp://send?phone=0040759454283';
+    } else {
+      // Dacă nu, deschide link-ul pentru laptop
+      window.open('https://wa.me/0040759454283', '_blank');
+    }
+  }
 }
