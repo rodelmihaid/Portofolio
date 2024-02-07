b37b82d77f884c9993a15ed30b7f6f5c5b72f0b6
rodelmihaid <dimuletemihai20@stud.ase.ro>
2024-02-07 16:18:28 +0200

    contact me v4 commit

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -10,10 +10,10 @@ export class ContactComponent {
     // Verifică dacă utilizatorul este pe un dispozitiv mobil
     if (/Mobi|Android/i.test(navigator.userAgent)) {
       // Dacă da, deschide link-ul pentru WhatsApp
-      window.location.href = 'whatsapp://send?phone=0040759454283';
+      window.location.href = 'whatsapp://send?phone=+40759454283';
     } else {
       // Dacă nu, deschide link-ul pentru laptop
-      window.open('https://wa.me/0040759454283', '_blank');
+      window.open('https://wa.me/+40759454283', '_blank');
     }
   }
 }
